94e61b09c78cc6bb009daccd7dc356cac1552bb0
MasonA <mason@ourprairie.net>
2018-10-17 16:31:17 -0700

    gif placeholders and positioning

--- src/components/gifslist.jsx
+++ src/components/gifslist.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import "../stylesheets/gifs.scss";
 
 const GifsList = props => {
+  const randomColor = () => {
+    const randNum = () => Math.ceil(Math.random() * 255);
+    return (`rgb(${randNum()}, ${randNum()}, ${randNum()})`)
+  }
+
   const gifs = props.gifs.map((gif, idx) => (
     <img
       className="gif"
+      style={{backgroundColor: randomColor()}}
       key={idx}
       src={gif.images.fixed_width.url}
       alt={gif.title}
